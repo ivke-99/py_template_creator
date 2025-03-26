acdff97257f56e71562237d95a3adb58190b1223
Ivan Djuraki <ivan.djuraki@bridgewaterlabs.com>
2025-03-26 20:33:21 +0100

    new rest api template fixes

--- py_template_creator/api-scelet/{{ cookiecutter.project_name }}/rest_api/package/app/models/__init__.py
+++ py_template_creator/api-scelet/{{ cookiecutter.project_name }}/rest_api/package/app/models/__init__.py
@@ -1,4 +1,17 @@
-from .base import Base, get_db_session, close_db_connection
+from .base import (
+    Base,
+    get_db_session,
+    close_db_connection,
+    DB_URL,
+    DatabaseSessionManager,
+)
 from .user import UsersModel
 
-__all__ = ["Base", "get_db_session", "UsersModel", "close_db_connection"]
+__all__ = [
+    "Base",
+    "get_db_session",
+    "UsersModel",
+    "close_db_connection",
+    "DB_URL",
+    "DatabaseSessionManager",
+]
